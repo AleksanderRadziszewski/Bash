e7787a658b7fc82d83cb376398b833a401b1fa13
Aleksander Radziszewski <aleksanderradziszewski@localhost.com>
2023-05-18 21:16:24 +0200

    validation of positional arguments, smtp config

---
+++ run_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+python3 $1 $2
+
+python_status=$?
+
+current_date=$(date "+%d/%m/%y %T")
+
+msg="The following Script ran to error: $1 at $current_date"
+
+if [ $python_status -ne 0 ]; then
+echo "$msg" | mail -v -s "Scipt Failure" wayefi3795@carpetra.com;
+
+else
+msg="A script has ran to error:
+Server: $HOSTNAME
+Script Name: $1
+Time: $current_date"
+
+fi;
+
+echo "$python_status"
+echo "$msg"
